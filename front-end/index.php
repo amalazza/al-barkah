ed86af9462eca6c621426cdb5c9ff4b4bf83ebe3
amalazza <dinaazza8@gmail.com>
2019-04-10 21:38:34 +0700

    first commit for responsive web design

---
+++ front-end/index.php
@@ -0,0 +1,303 @@
+<?php include "../back-end/config.php";?>
+
+<!DOCTYPE html>
+<!--
+Template Name: Basend
+Author: <a href="http://www.os-templates.com/">OS Templates</a>
+Author URI: http://www.os-templates.com/
+Licence: Free to use under our free template licence terms
+Licence URI: http://www.os-templates.com/template-terms
+-->
+<html lang="">
+<head>
+<title>Basend</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<link href="layout/styles/layout.css" rel="stylesheet" type="text/css" media="all">
+</head>
+<body id="top">
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- Top Background Image Wrapper -->
+<div class="bgded overlay" style="background-image:url('images/demo/backgrounds/01.jpg');"> 
+  <!-- ################################################################################################ -->
+  <div class="wrapper row1">
+    <header id="header" class="hoc clear"> 
+      <!-- ################################################################################################ -->
+      <div id="logo" class="fl_left">
+        <h1><a href="index.html">Al-Barkah Bekasi</a></h1>
+      </div>
+      <nav id="mainav" class="fl_right">
+        <ul class="clear">
+          <li class="active"><a href="index.html">Beranda</a></li>
+          <li><a class="drop" href="#">Al-Barkah</a>
+            <ul>
+              <li><a href="pages/gallery.html">Tentang</a></li>
+              <li><a href="pages/full-width.html">Management</a></li>
+              <li><a href="pages/sidebar-left.html">Galeri</a></li>
+            </ul>
+          </li>
+          <li><a class="drop" href="#">Kegiatan</a>
+            <ul>
+				<li><a href="#">Kajian</a></li>
+				<li><a href="#">Berita Terkini</a></li>
+				<li><a href="#">Galeri Kegiatan</a></li>
+            </ul>
+          </li>
+		  <li><a class="drop" href="#">Pelayanan</a>
+            <ul>
+				<li><a href="#">Bimbingan Mualaf</a></li>
+				<li><a href="#">Konsultasi Pernikahan dan Keluarga</a></li>
+            </ul>
+          </li>
+        </ul>
+      </nav>
+      <!-- ################################################################################################ -->
+    </header>
+  </div>
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <!-- ################################################################################################ -->
+  <section id="pageintro" class="hoc clear">
+    <div> 
+      <!-- ################################################################################################ -->
+      <h2 class="heading">Masjid Agung Al-Barkah Bekasi</h2>
+      <p>Masjid ini merupakan salah satu masjid tua di Indonesia. Masjid ini telah mengalami beberapa kali renovasi.</p>
+      <footer><a class="btn" href="#">Lebih lanjut</a></footer>
+      <!-- ################################################################################################ -->
+    </div>
+  </section>
+  <!-- ################################################################################################ -->
+</div>
+<!-- End Top Background Image Wrapper -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+
+<div class="wrapper row3">
+  <section class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="sectiontitle">
+      <h6 class="heading">BERITA TERKINI</h6>
+      <p>Menyajikan berita terkini seputar Masjid Agung Al-Barkah Bekasi</p>
+    </div>
+    <div class="group excerpts">
+      <article class="one_third first">
+        <figure><img src="images/demo/berita.jpg" alt="" style="x-high: 1">
+          <figcaption>
+            <time datetime="2045-04-06T08:15+00:00"><strong>06</strong> <em>Apr</em></time>
+          </figcaption>
+        </figure>
+        <div class="hgroup">
+          <h4 class="heading">Deklarasi Pemilu Damai di Halaman Masjid Al-Barkah</h4>
+          <ul class="nospace meta">
+            <li>by <a href="#">Admin</a></li>
+          </ul>
+        </div>
+        <div class="txtwrap">
+          <p>Forum Kerukunan Umat Beragama ( FKUB ) Jakarta Pusat bersama para tokoh agama dan pemerintah Kota Jakarta Pusat [&hellip;]</p>
+        </div>
+        <footer><a class="btn" href="#">Read More &raquo;</a></footer>
+      </article>
+      <article class="one_third">
+        <figure><img src="images/demo/berita2.jpg" alt="" style="x-high: 1">
+          <figcaption>
+            <time datetime="2045-04-05T08:15+00:00"><strong>05</strong> <em>Apr</em></time>
+          </figcaption>
+        </figure>
+        <div class="hgroup">
+          <h4 class="heading">Bhakti Merah Putih Narapidana di Masjid Al-Barkah</h4>
+          <ul class="nospace meta">
+            <li>by <a href="#">Admin</a></li>
+          </ul>
+        </div>
+        <div class="txtwrap">
+          <p>Dalam rangka memperingati Hari Bakti Pemasyarakatan ke-55 tahun 2019, Direktorat Jenderal Pemasyarakatan [&hellip;]</p>
+        </div>
+        <footer><a class="btn" href="#">Read More &raquo;</a></footer>
+      </article>
+      <article class="one_third">
+        <figure><img src="images/demo/berita3.jpg" alt="" style="width: 330px;">
+          <figcaption>
+            <time datetime="2045-04-04T08:15+00:00"><strong>04</strong> <em>Apr</em></time>
+          </figcaption>
+        </figure>
+        <div class="hgroup">
+          <h4 class="heading">KAJIAN ISLAM AHAD BERSAMA KETUA BPPMI DAN GUS MUWAFIQ</h4>
+          <ul class="nospace meta">
+            <li>by <a href="#">Admin</a></li>
+          </ul>
+        </div>
+        <div class="txtwrap">
+          <p>Masjid Al-Barkah mengadakan kajian Islam Ahad pada tanggal 2 Desember 2018 pukul 07.30 hingga selesai [&hellip;]</p>
+        </div>
+        <footer><a class="btn" href="#">Read More &raquo;</a></footer>
+      </article>
+    </div>
+    <!-- ################################################################################################ -->
+    <div class="clear"></div>
+  </section>
+</div>
+
+<div class="wrapper row2">
+  <section class="hoc container clear"> 
+    <!-- ################################################################################################ -->
+    <div class="sectiontitle">
+      <h6 class="heading">PELAYANAN</h6>
+      <p>Masjid Istiqlal selain sebagai pusat ibadah, juga memberikan pelayanan lain yang dapat dinikmati oleh masyarakat
+guna meningkatkan kualitas penghambaan kepada Allah Subhanahu Wa Ta’ala.</p>
+    </div>
+    <ul class="nospace group services">
+      <li class="one_quarter">
+        <article><a href="#"><i class="fa fa-3x fa-lastfm"></i></a>
+          <h6 class="heading font-x1"><a href="#">Bimbingan Mualaf</a></h6>
+          <p>Melayani bimbingan mualaf dengan segenap hati</p>
+        </article>
+      </li>
+      <li class="one_quarter">
+        <article><a href="#"><i class="fa fa-3x fa-puzzle-piece"></i></a>
+          <h6 class="heading font-x1"><a href="#">Konsultasi</a></h6>
+          <p>Melayani konsultasi pernikahan dan keluarga</p>
+        </article>
+      </li>
+    </ul>
+	
+	<table border="1" class="table">
+					<thead>
+						<tr>
+							<th>No</th>
+							<th>Nama</th>
+							<th>Alamat</th>
+							<th>Jenis Kelamin</th>
+							<th>Agama</th>
+							<th>Sekolah Asal</th>
+							
+							<th>Gambar</th>
+							<th>Nama Gambar</th>
+							<th>Ukuran Gambar</th>
+							<th>Tipe Gambar</th>
+							<th>Tindakan</th>
+						</tr>
+					</thead>
+					<tbody>
+						
+						<?php
+						$sql = "SELECT * FROM calon_siswa";
+						$query = mysqli_query($db, $sql);
+						
+						while($siswa = mysqli_fetch_array($query)){
+							echo "<tr>";
+							
+							echo "<td>".$siswa['id']."</td>";
+							echo "<td>".$siswa['nama']."</td>";
+							echo "<td>".$siswa['alamat']."</td>";
+							echo "<td>".$siswa['jenis_kelamin']."</td>";
+							echo "<td>".$siswa['agama']."</td>";
+							echo "<td>".$siswa['sekolah_asal']."</td>";
+							
+							echo "<td><img src='../back-end/images/".$siswa['nama_gambar']."' width='100' height='100'></td>";
+							echo "<td>".$siswa['nama']."</td>";
+							echo "<td>".$siswa['ukuran']."</td>";
+							echo "<td>".$siswa['tipe']."</td>";
+							
+							echo "<td>";
+							echo "<a href='form-edit.php?id=".$siswa['id']."'>Edit</a> | ";
+							echo "<a href='hapus.php?id=".$siswa['id']."'>Hapus</a>";
+							echo "</td>";
+							
+							echo "</tr>";
+						}		
+						?>
+						
+					</tbody>
+					</table>
+	
+	
+    <!-- ################################################################################################ -->
+    <div class="clear"></div>
+  </section>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+
+
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row4 bgded overlay" style="background-image:url('images/demo/backgrounds/02.png');">
+  <footer id="footer" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <div class="one_third first">
+      <h6 class="heading">Basend</h6>
+      <p>Nullam quis ligula elementum lectus varius aliquet vivamus odio donec metus libero semper quis suscipit ut aliquam a metus integer.</p>
+      <p class="btmspace-50">Pretium curabitur magna odio laoreet eu accumsan vitae gravida quis odio.</p>
+      <nav>
+        <ul class="nospace">
+          <li><a href="index.html"><i class="fa fa-lg fa-home"></i></a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+          <li><a href="#">Terms</a></li>
+          <li><a href="#">Privacy</a></li>
+          <li><a href="#">Cookies</a></li>
+          <li><a href="#">Disclaimer</a></li>
+          <li><a href="#">Online Shop</a></li>
+          <li><a href="#">Sitemap</a></li>
+        </ul>
+      </nav>
+    </div>
+    <div class="one_third">
+      <h6 class="heading">Aliquam non fermentum</h6>
+      <ul class="nospace btmspace-30 linklist contact">
+        <li><i class="fa fa-map-marker"></i>
+          <address>
+          Street Name &amp; Number, Town, Postcode/Zip
+          </address>
+        </li>
+        <li><i class="fa fa-phone"></i> +00 (123) 456 7890</li>
+        <li><i class="fa fa-fax"></i> +00 (123) 456 7890</li>
+        <li><i class="fa fa-envelope-o"></i> info@domain.com</li>
+      </ul>
+      <ul class="faico clear">
+        <li><a class="faicon-facebook" href="#"><i class="fa fa-facebook"></i></a></li>
+        <li><a class="faicon-twitter" href="#"><i class="fa fa-twitter"></i></a></li>
+        <li><a class="faicon-dribble" href="#"><i class="fa fa-dribbble"></i></a></li>
+        <li><a class="faicon-linkedin" href="#"><i class="fa fa-linkedin"></i></a></li>
+        <li><a class="faicon-google-plus" href="#"><i class="fa fa-google-plus"></i></a></li>
+        <li><a class="faicon-vk" href="#"><i class="fa fa-vk"></i></a></li>
+      </ul>
+    </div>
+    <div class="one_third">
+      <h6 class="heading">Sapien lobortis nec</h6>
+      <article><a href="#"><img class="btmspace-15" src="images/demo/320x140.png" alt=""></a>
+        <h6 class="nospace font-x1"><a href="#">Velit pellentesque</a></h6>
+        <time class="font-xs block btmspace-10" datetime="2045-04-05">Thursday, 5<sup>th</sup> April 2045</time>
+        <p class="nospace">Eget nisl mauris placerat mauris a semper posuere sem arcu cursus felis non cursus enim odio in maecenas ut justo [&hellip;]</p>
+      </article>
+    </div>
+    <!-- ################################################################################################ -->
+  </footer>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<div class="wrapper row5">
+  <div id="copyright" class="hoc clear"> 
+    <!-- ################################################################################################ -->
+    <p class="fl_left">Copyright &copy; 2016 - All Rights Reserved - <a href="#">Domain Name</a></p>
+    <p class="fl_right">Template by <a target="_blank" href="http://www.os-templates.com/" title="Free Website Templates">OS Templates</a></p>
+    <!-- ################################################################################################ -->
+  </div>
+</div>
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<!-- ################################################################################################ -->
+<a id="backtotop" href="#top"><i class="fa fa-chevron-up"></i></a>
+<!-- JAVASCRIPTS -->
+<script src="layout/scripts/jquery.min.js"></script>
+<script src="layout/scripts/jquery.backtotop.js"></script>
+<script src="layout/scripts/jquery.mobilemenu.js"></script>
+</body>
+</html>
